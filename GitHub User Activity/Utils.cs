e4adcede7ddb93da9c3ec85e2e2b16c58f5ed679
Gonzalo E <errandoneagonzalo18@gmail.com>
2025-01-30 13:25:16 -0300

    ToUpper the first letter FUNC

---
+++ GitHub User Activity/Utils.cs
@@ -0,0 +1,14 @@
+﻿namespace GitHub_User_Activity
+{
+    public static class Utils
+    {
+        public static string CapitalizeFirstLetter(string word)
+        {
+            if (string.IsNullOrEmpty(word))
+                return word;
+
+            return char.ToUpper(word[0]) + word.Substring(1);
+        }
+
+    }
+}
