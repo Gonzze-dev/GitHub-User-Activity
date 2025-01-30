b41128bed35c4335fec4c3712af3a716ca31ccfe
Gonzalo E <errandoneagonzalo18@gmail.com>
2025-01-30 11:09:54 -0300

    Agregar archivos de proyecto.

---
+++ GitHub User Activity/ApiService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub_User_Activity
+{
+    public static class ApiService
+    {
+        
+        static readonly HttpClient httpclient = new();
+    
+        public static async void get()
+        {
+            var username = "Gonzze-dev";
+            var url = $"https://api.github.com/users/{username}/events";
+
+            var response = await httpclient.GetAsync(url);
+
+            Console.WriteLine(response);
+        }
+    }
+}
