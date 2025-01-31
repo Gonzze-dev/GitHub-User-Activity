1e8eaaf8cd33c6b63dc9b1df288e088ffe046ec6
Gonzalo E <errandoneagonzalo18@gmail.com>
2025-01-31 17:44:55 -0300

    responsible for processing info

--- GitHub User Activity/ApiService.cs
+++ GitHub User Activity/ApiService.cs
@@ -1,90 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Text.Json;
-using System.Threading.Tasks;
-
-namespace GitHub_User_Activity
+﻿namespace GitHub_User_Activity
 {
     public static class ApiService
     {
-        
-        static readonly HttpClient _httpClient = new();
-        
+    
         public static async Task GetUserActivity()
         {
-            Dictionary<string, string> Events = new()
+
+            var contenJson = await UserRepository.GetUserActivity();
+           
+            foreach (var item in contenJson)
             {
-                {"PushEvent", "Push" },
-                {"PullRequestEvent",  "Pull"},
-                {"IssuesEvent", "Issued"},
-                {"IssueCommentEvent", "Issue comment"},
-                {"ForkEvent", "Fork"},
-                {"WatchEvent", "Watch"},
-                {"CreateEvent", "Create"},
-                {"DeleteEvent", "Delete"},
-                {"ReleaseEvent", "Release"},
-            };
-
-            List<string> EventsWithActions = [
-                "PullRequestEvent",
-                "IssuesEvent",
-                "IssueCommentEvent"
-            ];
-
-            var username = "kamranahmedse";
-            var url = $"https://api.github.com/users/{username}/events";
-
-            var request = new HttpRequestMessage(HttpMethod.Get, url)
-            {
-                Headers =
-                {
-                    {"Accept", "application/vnd.github+json" },
-                    {"User-Agent", "GitHub-User-Activity" }
-                }
-            };
-
-            var response = await _httpClient.SendAsync(request);
-
-            if (response.StatusCode == HttpStatusCode.NotFound)
-                throw new Exception($"Error, the username {username} no exist");
-            else if (!response.IsSuccessStatusCode)
-                throw new Exception($"Error in the request: {response.StatusCode}");
-            
-            var content = await response.Content.ReadAsStringAsync();
-            var contentJson = JsonDocument.Parse(content).RootElement.EnumerateArray();
-
-            foreach ( var item in contentJson )
-            {
-                var type = item.GetProperty("type");
                 var payload = item.GetProperty("payload");
-                var repoName = item.GetProperty("repo").GetProperty("name");
+                var resultCommit = payload.TryGetProperty("commits", out var commits);
 
-                var typeEvent = type.ToString();
-                var typeActionName = Events[typeEvent];
-
-                if (typeEvent == "PushEvent")
-                {
-                    var countCommits = payload.GetProperty("commits").EnumerateArray().Count();
-
-                    Console.WriteLine($"{typeActionName}ed {countCommits} in {repoName}");
-                }
-                else if (EventsWithActions.Contains(typeEvent))
-                {
-                    var action = payload.GetProperty("action").ToString();
-
-                    action = Utils.CapitalizeFirstLetter(action);
-                    typeActionName = typeActionName.ToLower();
-
-                    Console.WriteLine($"{action} a new {typeActionName} in {repoName}");
-                }
-                else
-                {
-                    Console.WriteLine($"{typeActionName}ed in {repoName}");
-                }
+                var resultActionName = payload.TryGetProperty("action", out var actionName);
                 
+                EventMessageData eMessageData = new()
+                {
+                    TypeEvent = item.GetProperty("type").ToString(),
+                    ActionName = resultActionName ? actionName.ToString() : "",
+                    RepoName = item.GetProperty("repo").GetProperty("name").ToString(),
+                    Commits = resultCommit ? commits.EnumerateArray().Count() : 0
+                };
+
+                var message = EventMessageFormatter.GetMessageFormatted(eMessageData);
+
+                Console.WriteLine(message);
             }
 
             
