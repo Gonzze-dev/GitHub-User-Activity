1482b3f014aac9ffc2d33f6dd5948142bf51bce0
Gonzalo E <errandoneagonzalo18@gmail.com>
2025-01-31 17:44:02 -0300

    Obtain only data

---
+++ GitHub User Activity/UserRepository.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace GitHub_User_Activity
+{
+    public static class UserRepository
+    {
+        static readonly HttpClient _httpClient = new();
+        
+        static string Url (string username) => 
+            $"https://api.github.com/users/{username}/events";
+
+        public static async Task<JsonElement.ArrayEnumerator> GetUserActivity(string username = "kamranahmedse")
+        {
+            var url = Url(username);
+            
+            var request = new HttpRequestMessage(HttpMethod.Get, url)
+            {
+                Headers =
+                {
+                    {"Accept", "application/vnd.github+json" },
+                    {"User-Agent", "GitHub-User-Activity" }
+                }
+            };
+
+            var response = await _httpClient.SendAsync(request);
+
+            if (response.StatusCode == HttpStatusCode.NotFound)
+                throw new Exception($"Error, the username {username} no exist");
+            else if (!response.IsSuccessStatusCode)
+                throw new Exception($"Error in the request: {response.StatusCode}");
+
+            var content = await response.Content.ReadAsStringAsync();
+            var contentJson = JsonDocument.Parse(content).RootElement.EnumerateArray();
+
+            return contentJson;
+        }
+
+    }
+}
