69ac2cd0d57daebeca4fdb8d213f194ecac226b4
Gonzalo E <errandoneagonzalo18@gmail.com>
2025-01-31 17:45:49 -0300

    Git JsonStructure data

---
+++ GitHub User Activity/EventMessageData.cs
@@ -0,0 +1,10 @@
+﻿namespace GitHub_User_Activity
+{
+    public class EventMessageData
+    {
+        public string TypeEvent { get; set; }
+        public string ActionName { get; set; }
+        public string RepoName { get; set; }
+        public int Commits { get; set; }
+    }
+}
