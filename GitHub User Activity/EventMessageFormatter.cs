057ff226cdaea9bff5935860c498e3d9a8465c8c
Gonzalo E <errandoneagonzalo18@gmail.com>
2025-02-01 12:36:23 -0300

    DEL import useless

--- GitHub User Activity/EventMessageFormatter.cs
+++ GitHub User Activity/EventMessageFormatter.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Collections.Specialized.BitVector32;
-
-namespace GitHub_User_Activity
+﻿namespace GitHub_User_Activity
 {
     public static class EventMessageFormatter
     {
